100231f23fc2aa11fa354ffea4ee03bbf608b21a
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-03 14:23:38 +0200

    add exercise11

---
+++ Modulo-2-JavaScript/Intro-Programacion2/Ejercicio-11/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log(2 + 3 + '5');
+
+console.log('2' + 3 + 5);
+
+console.log(123);
+
+console.log('123');
+
+//no ocurre lo mismo en el primer ejemplo (55) que en el segundo (235), en el tercero salen los números en azul
