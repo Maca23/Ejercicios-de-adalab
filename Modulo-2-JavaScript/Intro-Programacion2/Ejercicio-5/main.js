4f24ded72dd496a93e0802939dd50a89693b5c22
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-03 10:44:19 +0200

    exercise5

---
+++ Modulo-2-JavaScript/Intro-Programacion2/Ejercicio-5/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const age = 33;
+const hoursDay = 24;
+const daysYear = 365;
+
+const totalHoursLife = (age * hoursDay * daysYear);
+
+console.log('Total horas vividas: ' + totalHoursLife + 'horas vividas');
