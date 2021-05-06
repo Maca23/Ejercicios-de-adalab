31563c2ffa0c4665fb73221b7c2cac88675c4dd6
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-06 08:21:28 +0200

    completed exercise4

--- Modulo-2-JavaScript/4-Funciones/Ejercicio-4/main.js
+++ Modulo-2-JavaScript/4-Funciones/Ejercicio-4/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function num(a) {
-    if (num  ) {
-        return 'El número es par'
-    }else
+    if (a %2 === 0) {
+        return 'El número es par';
+    }else{
         return 'El número es impar'
+    }
 }
+
+const result = num (2);
+console.log(result);
