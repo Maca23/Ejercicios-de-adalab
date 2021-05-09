5d2556e4ad331b35209a191a8c0f24cef2b39348
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-09 09:43:45 +0200

    good exercise2-7

--- Modulo-2-JavaScript/2-Intro-Programacion2/Ejercicio-7/main.js
+++ Modulo-2-JavaScript/2-Intro-Programacion2/Ejercicio-7/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const name = 'Rocío';
-const text = 'encantada de conocerte.'
+const namePair = 'Rocío';
+const h1 = document.querySelector('h1');
 
-console.log(`Hola ${name}, ${text}`);
+h1.innerHTML = `Hola ${namePair}, encantada de conocerte.`;
