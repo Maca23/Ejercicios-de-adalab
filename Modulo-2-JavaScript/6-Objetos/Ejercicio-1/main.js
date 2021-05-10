c9836614270e22a4824f6cf6e5f5fb134fefc212
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-10 07:51:03 +0200

    add exercise 6-1

---
+++ Modulo-2-JavaScript/6-Objetos/Ejercicio-1/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const adalaber1 = {
+    name: "Susana",
+    age: 34,
+    job: "periodista",
+};
+
+const pAdalaber1 = document.querySelector(".adalaber1");
+pAdalaber1.innerHTML = 'Mi nombre es ' + adalaber1.name + ', tengo ' + adalaber1.age + ' años y soy ' + adalaber1.job;
+
+const adalaber2 = {
+    name: "Rocío",
+    age: 25,
+    job: "actriz",
+};
+
+const pAdalaber2 = document.querySelector(".adalaber2");
+pAdalaber2.innerHTML = 'Mi nombre es ' + adalaber2.name + ', tengo ' + adalaber2.age + ' años y soy ' + adalaber2.job;
