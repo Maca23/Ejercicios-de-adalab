0361b72f3e93b6dd3103a7eba4f8d687b327c320
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-10 16:27:26 +0200

    add exercise 7-1

---
+++ Modulo-2-JavaScript/7-Arrays-y-bucles/Ejercicio-1/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function workWithMovies() {
+    const movies = ['Titanic', 'Spiderman', 'Pocahontas'];
+    movies[3] = 'Superman';
+    movies[1] = 'Tarzán';
+    movies[0] = 'Wonderwoman';
+};
+
+console.log(workWithMovies);
