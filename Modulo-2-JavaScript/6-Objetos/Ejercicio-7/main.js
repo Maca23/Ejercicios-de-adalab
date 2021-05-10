a7c77d8e3414969b71a6c47f5827f380cc6cab22
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-10 11:25:46 +0200

    add exercise 6-7

---
+++ Modulo-2-JavaScript/6-Objetos/Ejercicio-7/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const object = {};
+const job = 'developer';
+
+object.firstName = 'Macarena';
+object.lastName = 'Gonzalez';
+object.age = 34;
+object.job = job;
+object.age = object.age+1;
+
+console.log(object.firstName);
+console.log(object.lastName);
+console.log(object.age);
+console.log(object.job);
