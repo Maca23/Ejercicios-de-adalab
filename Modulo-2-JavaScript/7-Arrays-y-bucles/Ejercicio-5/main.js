6c91ffb0192df2d26d37bb0142e8f192ae456fa0
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-10 18:35:29 +0200

    incompleted exercise 7-6

--- Modulo-2-JavaScript/7-Arrays-y-bucles/Ejercicio-5/main.js
+++ Modulo-2-JavaScript/7-Arrays-y-bucles/Ejercicio-5/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* PRIMERA PARTE DEL EJERCICIO
+
 let numbers = [3, 5, 1, 8, 9];
 
 let acc = 0;
@@ -8,4 +10,29 @@ for (let i = 0; i < numbers.length; i++) {
     acc += numbers[i]/5;
 }
 
-console.log('La media es: ' + acc);
+console.log('La media es ' + acc);*/
+
+
+/* SEGUNDA PARTE DEL EJERCICIO
+let numbers2 = [3, 5, 1, 8, 9, 2];
+
+let acc2 = 0;
+
+for (let i = 0; i < numbers2.length; i++) {
+    acc2 += numbers2[i]/6;
+}
+
+console.log('La media es ' + acc2);*/
+
+let numbers = [3, 5, 1, 8, 9];
+let acc = 0;
+
+function average(numbers){
+    for (let i = 0; i<numbers.length;i++){
+        acc += numbers[i];
+    }
+    let media = (acc/numbers.length);
+    console.log('La media es '+ media);
+}
+
+const result = average(numbers);
